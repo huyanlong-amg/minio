4dd117903d72c62fce327f45f8485a358fcb6ea7
huyanlong <2536620695@qq.com>
2021-04-07 23:53:21 +0800

    minio文件上传、删除等等

--- src/main/java/com/hyl/minio/controller/MinIoController.java
+++ src/main/java/com/hyl/minio/controller/MinIoController.java
@@ -4,6 +4,9 @@ import com.hyl.minio.util.MinioUtils;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +24,17 @@ public class MinIoController {
     @Autowired
     MinioUtils inioUtils;
 
+    @Value("${minio.bucket}")
+    String bucket;
+
     @PostMapping("/upload")
-    public String upload(@RequestParam("data") MultipartFile data) throws Exception{
+    public String upload(@RequestParam("data") MultipartFile data, @RequestParam(name = "bucketName", required = false) String bucketName) throws Exception{
+        if (!StringUtils.hasLength(bucketName)) {
+            bucketName = bucket;
+        }
         String fileName = data.getOriginalFilename();
         InputStream inputStram = data.getInputStream();
-        inioUtils.upload("huyanlong", fileName, inputStram);
+        inioUtils.upload(bucketName, fileName, inputStram);
         return "上传成功";
     }
 }
